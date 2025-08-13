ca7d90d2518bf10c2382417a973cc0f6004bd9c2
Haytam ELGBOURI <130654725+Haytam-Nagi@users.noreply.github.com>
2025-08-13 01:13:58 +0100

    added ligne total price to dto to get it in the frontend

--- src/main/java/com/scalux/stockManagement/dtos/BCLineDTO.java
+++ src/main/java/com/scalux/stockManagement/dtos/BCLineDTO.java
@@ -3,9 +3,12 @@ package com.scalux.stockManagement.dtos;
 import com.scalux.stockManagement.entities.Article;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class BCLineDTO {
     private ArticleDTO article;
     private String color;
     private Integer quantity;
+    private BigDecimal prixTotalLigne;
 }
